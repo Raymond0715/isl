76bdf8194a786d5c4079f252eb348c81e5b034b7
Stefan Ganser <ganser@fim.uni-passau.de>
2016-01-19 12:15:31 +0100

    Exported isl_basic_map_remove_redundancies and isl_val_get_num_si

--- include/isl/map.h
+++ include/isl/map.h
@@ -129,6 +129,8 @@ __isl_give isl_basic_map *isl_basic_map_more_at(__isl_take isl_space *dim,
 __isl_give isl_basic_map *isl_basic_map_empty(__isl_take isl_space *dim);
 __isl_give isl_basic_map *isl_basic_map_universe(__isl_take isl_space *dim);
 __isl_give isl_basic_map *isl_basic_map_nat_universe(__isl_take isl_space *dim);
+
+__isl_export
 __isl_give isl_basic_map *isl_basic_map_remove_redundancies(
 	__isl_take isl_basic_map *bmap);
 __isl_give isl_map *isl_map_remove_redundancies(__isl_take isl_map *map);
