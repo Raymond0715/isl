66392d8b454dcbdec8d584f434464f7965a8bb6d
Stefan Ganser <ganser@fim.uni-passau.de>
2017-01-13 12:25:38 +0100

    exporting isl_map_n_basic_map

--- include/isl/map.h
+++ include/isl/map.h
@@ -778,7 +778,7 @@ isl_bool isl_map_plain_is_equal(__isl_keep isl_map *map1,
 	__isl_keep isl_map *map2);
 
 uint32_t isl_map_get_hash(__isl_keep isl_map *map);
-
+__isl_export
 int isl_map_n_basic_map(__isl_keep isl_map *map);
 __isl_export
 isl_stat isl_map_foreach_basic_map(__isl_keep isl_map *map,
